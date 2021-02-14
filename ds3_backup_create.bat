6f5ff24900dabd4af7b35cd77a307f0b4d5c395b
aloofbit <58625258+aloofbit@users.noreply.github.com>
2021-02-14 03:37:29 -0600

    Update ds3_backup_create.bat

--- ds3_backup_create.bat
+++ ds3_backup_create.bat
@@ -3,9 +3,13 @@
 : convention
 
 SET HOUR=%time:~0,2%
-SET dtStamp9=%date:~-4%-%date:~4,2%-%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2%
-SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+SET dtStamp9=%date:~-4%-%date:~4,2%-%date:~7,2%_0%time:~1,1%_%time:~3,2%
+SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%
 
 if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
 
-copy DS30000.sl2 DS30000_%dtStamp%.sl2
+@echo off
+set /p id="Backup name: "
+
+echo
+copy DS30000.sl2 %dtStamp%_%id%.sl2
